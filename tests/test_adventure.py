74f0df22e3f6e00121facd644c60e6a4b822a796
netserf <greg.horie@telus.com>
2019-09-27 21:14:26 -0600

    adding the intro to the story which also sets the initial hero stats

--- tests/test_adventure.py
+++ tests/test_adventure.py
@@ -1,4 +1,5 @@
 import python_adventure.adventure as adventure
+import python_adventure.characters as chars
 import pytest
 from unittest.mock import patch
 import os
@@ -26,3 +27,20 @@ def test_get_direction_invalid(mocker, capsys):
     adventure.get_direction()
     captured = capsys.readouterr()
     assert 'try again' in captured.out
+
+def test_intro_typical(mocker):
+    mocker.patch('builtins.input', side_effect=['y', 'y'])
+    mocker.patch('time.sleep')  # no need to sleep when testing
+    hero = chars.Hero('Butch')
+    adventure.intro(hero)
+    assert 'sword' in hero.weapons
+    assert 'shield' in hero.armour
+
+def test_intro_joke(mocker):
+    mocker.patch('builtins.input', side_effect=['y', 'n'])
+    mocker.patch('time.sleep')  # no need to sleep when testing
+    hero = chars.Hero('Pepto')
+    adventure.intro(hero)
+    assert len(hero.weapons) == 0
+    assert len(hero.armour) == 0
+
